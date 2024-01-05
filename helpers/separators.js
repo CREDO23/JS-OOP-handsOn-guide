e0a1f9c6b0b36112c8466f02503f69bb5b4b3661
CREDO23 <thierrybakera12@gmail.com>
2024-01-05 20:07:04 +0200

    refact: fix gramar issues

--- helpers/separators.js
+++ helpers/separators.js
@@ -1,3 +1,6 @@
+// Here lives separators used for clarity and readability purposes in the console.
+// [TODO] : Separator rule's that can be used directly  in files and from them create a formater. 
+
 export const s1 = "\n";
 export const s2 = "\n \n";
 export const s3 = "\n \n \n";
--- helpers/styles.js
+++ helpers/styles.js
@@ -1,21 +1,20 @@
-// ======= Colors =========
-
 import styleParser from "./styleParser.js";
 
 /**
- * Here are the function's short names for all the styles I am using in the console.
- * Here short names are used for simplicity purposes.
+ * Here are the function's short names for all the styles used in the console.
  * 
  * 
  * LINE NUMBER STYLE [LEVEL] -> lns1
- * SUCCESS OUTPUT STYLE -> ss
- * ERROR OUTPUT STYLE -> es
+ * POSITIVE OUTPUT STYLE -> ps
+ * NEGATIVE OUTPUT STYLE -> ns
+ * 
+ * Short names are used for simplicity purposes.
  */
 
 export const lns1 = styleParser(["bgGreen", "bold"]);
 
 export const lns2 = styleParser(["bgGreen"]);
 
-export const ss = styleParser(["green"]);
+export const ps = styleParser(["green"]);
 
-export const es = styleParser(["red"]);
+export const ns = styleParser(["red"]);
