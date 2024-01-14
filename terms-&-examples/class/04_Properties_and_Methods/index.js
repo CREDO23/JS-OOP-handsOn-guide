c04fe829b0e347758387f5239d89c07cb00d74ef
CREDO23 <thierrybakera12@gmail.com>
2024-01-14 22:56:03 +0200

    feat: add properties & methods for class ter

---
+++ terms-&-examples/class/04_Properties_and_Methods/index.js
@@ -0,0 +1,16 @@
+import { ln } from "../../../helpers/getLineNumber.js"
+import { lns1 , lns2, ps, ns} from "../../../helpers/styles.js"
+import { s1, s4 } from "../../../helpers/separators.js"
+
+
+
+// ======= STATIC PROPERTIES / METHODS =======
+
+
+// ======= PROTECTED PROPERTIES / METHODS =======
+
+
+// ======= PRIVATE PROPERTIES / METHODS =======
+
+
+// ======= PUBLIC PROPERTIES / METHODS =======
